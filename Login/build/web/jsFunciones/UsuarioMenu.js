6b7606f8abf427c405d27c048f6f180d193ebac5
lolxdhola78 Gutierrez <gutierrez.flores.cristian.2006@gmail.com>
2024-10-07 00:11:24 -0600

    Login

---
+++ Login/build/web/jsFunciones/UsuarioMenu.js
@@ -0,0 +1,15 @@
+    // Al cargar la página, oculta el menú por defecto
+    document.getElementById("opciones").style.display = "none";
+
+    function mostrarOpciones() {
+        var opciones = document.getElementById("opciones");
+        opciones.style.display = (opciones.style.display === "none") ? "block" : "none";
+    }
+
+    function cargarLogin() {
+        // Acción para cargar el inicio de sesión
+    }
+
+    function cargarAltaUsuario() {
+        // Acción para cargar el registro de usuarios
+    }
